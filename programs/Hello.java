01426f0df6fad0ab0c7d70f33be7f5c794f892eb
Cameron Moberg <cjmoberg@gmail.com>
2016-01-12 15:46:08 -0600

    Update Hello.java

--- programs/Hello.java
+++ programs/Hello.java
@@ -1,1 +1,12 @@
+/**Basic Hello World Program
+ * @author Cameron Moberg, cjm8713
+ * @date 01/11/2016
+ */
 
+public class Hello_cjm8713
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello, world!");
+   }
+}
