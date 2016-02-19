cdd0bd3b261326732cf7f21719156281f3b2fe75
Cameron Moberg <cjmoberg@gmail.com>
2016-02-19 10:21:55 -0600

    Update SafeWithdrawlRate.java

--- programs/swr/SafeWithdrawlRate.java
+++ programs/swr/SafeWithdrawlRate.java
@@ -1,7 +1,3 @@
-//Sample command line arguments "-s 0.45 -b 0.25 -i 0.25 -w 0.04 -n 30 -f MarketData.csv"
-
-package swr;
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,113 +8,190 @@ import java.util.Scanner;
 
 public class SafeWithdrawalRate
 {
-   public static void main(String[] args) throws IOException
-   {
-      Portfolio portfolio = new Portfolio(args);
-      String fileName = portfolio.getMarketHistoryFileName();
-      Double[][] fileInfoArray = new Double[countLines(fileName) - 1][4];
+	public static void main(String[] args) throws IOException
+	{
+		Portfolio portfolio = new Portfolio();
+		parseArgs(args, portfolio);
 
-      portfolio.setCashPercent(1 - (portfolio.getBondPercent()
-            + portfolio.getInflationLinkedPercent()
-            + portfolio.getStockPercent()));
+		String fileName = portfolio.getMarketHistoryFileName();
+		int yearsInHistory = countLines(fileName) - 1;
 
-      if (portfolio.getCashPercent() < 0.0)
-      {
-         System.err.println("The percents add up to above 100%. Try Again.");
-         System.exit(1);
-      }
+		portfolio.setCashPercent(1.0 - (portfolio.getBondPercent()
+				+ portfolio.getInflationLinkedPercent()
+				+ portfolio.getStockPercent()));
+		portfolio.checkLegal();
 
-      portfolio.printInfo();
+		Double[][] fileInfoArray = new Double[yearsInHistory][4];
+		fileInfoArray = loadArray(fileName, fileInfoArray);
 
-      try
-      {
-         fileInfoArray = loadArray(fileName, fileInfoArray);
-      } catch (FileNotFoundException e)
-      {
-         e.printStackTrace();
-         System.out.println("File could not be opened for reading.");
-      }
+		portfolio.printInfo();
+		System.out.printf("==> %.2f%% probability of success!%n",
+				calculateSuccess(yearsInHistory, portfolio, fileInfoArray));
+	}
 
-      System.out.printf("==> %.2f%% probability of success!",
-            calculateSuccess(fileName, portfolio, fileInfoArray));
+	/**
+	 * Prints the required arguments for the program to run successfully.
+	 */
+	public static void printRequiredArgs()
+	{
+		System.out.println("-f <filename>      File containing historical data.");
+		System.out.println("-b <bonds>      Portfolio percent in bonds.");
+		System.out.println("-s <stocks>      Portfolio percent in stocks.");
+		System.out.println("-i <infl>    Portfolio percent in inflation linked.");
+		System.out.println("-w <withdraw>      Initial withdrawal rate.");
+		System.out.println("-n <years>      Years of retirement.");
+		System.out.println("-h <help>       Prints the help and all arguments.");
+		System.out.printf("%n Returns the historical success of a portfolio.%n");
+	}
 
-   }
+	/**
+	 * Simulates retirement success based upon historical data.
+	 * 
+	 * @param yearsInHistory
+	 *           Number of years of history to be used.
+	 * @param portfolio
+	 *           Object with data needed to simulate.
+	 * @param fileInfoArray
+	 *           Array that has all
+	 * @return Percent chance of success.
+	 * @throws IOException
+	 */
+	public static Double calculateSuccess(int yearsInHistory,
+			Portfolio portfolio, Double[][] fileInfoArray) throws IOException
+	{
+		Double portfolioPercent;
+		Double withdrawalAmount;
+		int count = 0;
+		int totalCount = 0;
 
-   public static Double calculateSuccess(String fileName, Portfolio portfolio,
-         Double[][] fileInfoArray) throws IOException
-   {
-      Double portfolioPercent;
-      Double withdrawalAmount;
-      int count = 0;
-      int totalCount = 0;
+		for (int startYear = 0; startYear <= yearsInHistory
+				- portfolio.getRetirementLength(); startYear++)
+		{
+			// reset portfolio stats
+			portfolioPercent = 1.0;
+			withdrawalAmount = portfolio.getInitialWithdrawalAmount();
 
-      for (int startYear = 0; startYear < countLines(fileName)
-            - portfolio.getRetirementLength(); startYear++)
-      {
-         portfolioPercent = 1.0;
-         withdrawalAmount = portfolio.getInitialWithdrawalAmount();
+			for (int year = startYear; year < startYear
+					+ portfolio.getRetirementLength() & portfolioPercent > 0; year++)
+			{
+				portfolioPercent = (portfolioPercent - withdrawalAmount)
+						+ portfolio.yearProfit(fileInfoArray, withdrawalAmount, year,
+								portfolioPercent);
+				withdrawalAmount *= (fileInfoArray[year][2] + 1.0);
+			}
 
-         for (int year = startYear; year < startYear
-               + portfolio.getRetirementLength() & portfolioPercent > 0; year++)
-         {
-            portfolioPercent = (portfolioPercent - withdrawalAmount)
-                  + portfolio.yearProfit(fileInfoArray, withdrawalAmount, year,
-                        portfolioPercent);
-            withdrawalAmount *= (fileInfoArray[year][2] + 1.0);
-         }
-         totalCount++;
-         if (portfolioPercent > 0)
-         {
-            count++;
-         }
-      }
-      return (100.0 * count / totalCount);
-   }
+			if (portfolioPercent > 0)
+				count++;
+			totalCount++;
+		}
+		return (100.0 * count / totalCount);
+	}
 
-   public static Double[][] loadArray(String fileName, Double[][] fileInfoArray)
-         throws FileNotFoundException
-   {
-      int i = 0;
-      Scanner inFile = new Scanner(new File(fileName));
+	public static void parseArgs(String[] args, Portfolio portfolio)
+	{
+		if (args.length != 12)
+		{
+			System.out.println("Please enter the correct number of arguments");
+			System.exit(1);
+		}
+		for (int i = 0; i < args.length; i++)
+		{
+			if (args[i].charAt(0) == '-')
+			{
+				switch (args[i].charAt(1))
+				{
+				case 'h':
+					printRequiredArgs();
+				case 'f':
+					portfolio.setMarketHistoryFileName(args[i + 1]);
+					break;
+				case 'b':
+					portfolio.setBondPercent(Double.parseDouble(args[i + 1]));
+					break;
+				case 's':
+					portfolio.setStockPercent(Double.parseDouble(args[i + 1]));
+					break;
+				case 'i':
+					portfolio.setInflationLinkedPercent(
+							Double.parseDouble(args[i + 1]));
+					break;
+				case 'w':
+					portfolio.setInitialWithdrawalAmount(
+							Double.parseDouble(args[i + 1]));
+					break;
+				case 'n':
+					portfolio.setRetirementLength(Integer.parseInt(args[i + 1]));
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
 
-      inFile.useDelimiter(",|\\n");
-      inFile.nextLine(); // To skip the first line.
-      while (inFile.hasNextDouble())
-      {
-         for (int j = 0; j < 4; j++)
-         {
-            fileInfoArray[i][j] = inFile.nextDouble();
-         }
-         i++;
-      }
-      inFile.close();
-      return fileInfoArray;
-   }
+	/**
+	 * Adds data to array from file.
+	 * 
+	 * @param fileName
+	 *           Name of file to read from.
+	 * @param fileInfoArray
+	 *           The array to load info into.
+	 * @return Returns 2D array loaded with data.
+	 * @throws FileNotFoundException
+	 */
+	public static Double[][] loadArray(String fileName, Double[][] fileInfoArray)
+			throws FileNotFoundException
+	{
+		int i = 0;
+		Scanner inFile = new Scanner(new File(fileName));
 
-   public static int countLines(String filename) throws IOException
-   {
-      InputStream is = new BufferedInputStream(new FileInputStream(filename));
-      try
-      {
-         byte[] c = new byte[1024];
-         int count = 0;
-         int readChars = 0;
-         boolean empty = true;
-         while ((readChars = is.read(c)) != -1)
-         {
-            empty = false;
-            for (int i = 0; i < readChars; ++i)
-            {
-               if (c[i] == '\n')
-               {
-                  ++count;
-               }
-            }
-         }
-         return (count == 0 && !empty) ? 1 : count;
-      } finally
-      {
-         is.close();
-      }
-   }
+		inFile.useDelimiter(",|\\n");
+		inFile.nextLine(); // To skip the first line.
+		while (inFile.hasNextDouble())
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				fileInfoArray[i][j] = inFile.nextDouble();
+			}
+			i++;
+		}
+		inFile.close();
+		return fileInfoArray;
+	}
+
+	/**
+	 * counts number of lines in a file by checking for newline character.
+	 * 
+	 * @author martinus, a user on Stack Overflow
+	 * @param filename
+	 *           The name of the file to count lines.
+	 * @return Returns number of lines in file.
+	 * @throws IOException
+	 */
+	public static int countLines(String filename) throws IOException
+	{
+		InputStream is = new BufferedInputStream(new FileInputStream(filename));
+		try
+		{
+			byte[] c = new byte[1024];
+			int count = 0;
+			int readChars = 0;
+			boolean empty = true;
+			while ((readChars = is.read(c)) != -1)
+			{
+				empty = false;
+				for (int i = 0; i < readChars; ++i)
+				{
+					if (c[i] == '\n')
+					{
+						++count;
+					}
+				}
+			}
+			return (count == 0 && !empty) ? 1 : count;
+		} finally
+		{
+			is.close();
+		}
+	}
 }
